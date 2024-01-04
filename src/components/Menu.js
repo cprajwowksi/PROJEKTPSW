131db586ebea2b4ca7d09bfa6ccb38c1ab4fc063
cprajwowski <c.prajwowski.631@studms.ug.edu.pl>
2024-01-04 14:26:21 +0100

    Rozpoczecie prac nad Panda Ramen Sushi

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+function Menu() {
+    return (
+        <div className="menu">
+            <div className="ramen-card card">
+                <p>Ramen</p>
+            </div>
+            <div className="sushi-card card">
+                <p>Sushi</p>
+            </div>
+        </div>
+    );
+}
+
+export default Menu;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Menu from '../components/Menu'
+import Nav from "../components/Nav";
+function Home() {
+    return (
+        <div className="homepage">
+            <Nav/>
+            <Menu/>
+        </div>
+    );
+}
+
+export default Home;
