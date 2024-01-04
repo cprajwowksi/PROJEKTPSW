b3d392e6169eec7d6d699c7401677aea09225b50
cprajwowski <c.prajwowski.631@studms.ug.edu.pl>
2024-01-04 17:32:10 +0100

    poczatek projektu, stworzenie stylu i pomyslu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,21 @@
 function Menu() {
     return (
         <div className="menu">
-            <div className="ramen-card card">
-                <p>Ramen</p>
+            <div className="menu-ramen menu-card">
+                <button className="ramen-button menu-button">
+                    <div className="ramen-card card">
+                    </div>
+                </button>
+                <h2>Ramen</h2>
             </div>
-            <div className="sushi-card card">
-                <p>Sushi</p>
+            <div className="menu-sushi menu-card">
+                <button className="sushi-button menu-button">
+                    <div className="sushi-card card">
+
+                    </div>
+
+                </button>
+                <h2>Sushi</h2>
             </div>
         </div>
     );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,19 @@
+import logo from '../img/image.png'
+import fblogo from '../img/fblogo.png'
+import iglogo from '../img/iglogo.png'
+
 function Nav() {
     return (
         <nav>
+            <button>ZALOGUJ</button>
+            <button><img className="fb-logo" src={fblogo} alt="ig-logo"/></button>
+            <button><img className="ig-logo" src={iglogo} alt="ig-logo"/></button>
             <div className="logo-container">
-                <img className="logo"/>
+                <img className="panda-logo" src={logo} alt="panda-logo"/>
             </div>
+            <button>CONTACT +48 605412523 &#9742; </button>
+            <button>ZAMOW</button>
+
 
         </nav>
     );
